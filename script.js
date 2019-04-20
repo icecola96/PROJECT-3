e7f123b52edb0620202375dab43a4e63a7d07f9c
Lola <oegbeyemi@hawk.iit.edu>
2019-04-19 23:43:42 -0500

    map js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 42.047778, lng: -87.756829},
-    zoom: 20
-  });
-}
+	var loc = {lat: 41.892654, lng: -87.610168};
+	var map = new google.maps.Map(document.getElementById('map'), {center: loc, zoom: 8});
 
+	var infowindow = new google.maps.InfoWindow({content: 'Navy Pier'});
+
+	var marker = new google.maps.Marker({position: loc,map: map,icon: 'media/location.jpg'});
+	
+	marker.addListener('click', function() {
+		infowindow.open(map, marker);
+	});
+}
